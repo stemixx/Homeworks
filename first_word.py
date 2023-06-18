80125dc809ba3f7e67c5b1713e6095ca6a9deb30
stemixx <stemix@mail.ru>
2023-06-18 19:37:59 +0300

    another quick version

--- first_word.py
+++ first_word.py
@@ -10,15 +10,22 @@ def first_word(text: str) -> str:
             break
     return (''.join(first_word))
 
+def first_word2(text: str) -> str:
+    return text.split(' ')[0]
 
 if __name__ == "__main__":
-    print("Example:")
+    print(f'Example first_word("Hello world"):')
     print(first_word("Hello world"))
+    print(f'Example first_word2("Hello world"):')
+    print(first_word2("Hello world"))
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert first_word("Hello world") == "Hello"
     assert first_word("a word") == "a"
     assert first_word("hi") == "hi"
 
+    assert first_word2("Hello world") == "Hello"
+    assert first_word2("a word") == "a"
+    assert first_word2("hi") == "hi"
 
 
